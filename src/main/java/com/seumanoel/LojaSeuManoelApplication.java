528f88db52272cb60196da8144f33b013c6c22f5
Jorge Java Sucesso <jorgelr.java@gmail.com>
2025-05-29 07:25:07 -0300

    Projeto inicial Spring Boot para API da loja do Seu Manoel

---
+++ src/main/java/com/seumanoel/LojaSeuManoelApplication.java
@@ -0,0 +1,13 @@
+package com.seumanoel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaSeuManoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaSeuManoelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seumanoel/LojaSeuManoelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seumanoel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaSeuManoelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
